fa9168fbb6cf06ef9695f309cf868aa00fc19bcf
Bartosz Zbik <bartosz@zbik.org>
2022-10-20 11:25:16 +0100

    correct ID to Name in description

--- bot_actions/my_name.py
+++ bot_actions/my_name.py
@@ -5,4 +5,4 @@ class MyName:
     """Sends users ID"""
 
     async def action_myname(self, message: discord.Message, core_message: str) -> None:
-        await message.channel.send(f"Your ID is: {message.author.name}")
+        await message.channel.send(f"Your name is: {message.author.name}")
