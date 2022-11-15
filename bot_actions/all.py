1713052e899fd5100e41cb3f1ffde2e6803db1ef
Bartosz Zbik <bartosz@zbik.org>
2022-11-15 15:12:10 +0100

    remove useless files

--- bot_actions/all.py
+++
@@ -1,7 +0,0 @@
-from bot_actions.my_id import MyId
-from bot_actions.my_name import MyName
-from bot_actions.speak import Speak
-
-class BotActions(MyId, MyName, Speak):
-    """Inherits all functionalities the bot has implemented"""
-    pass
