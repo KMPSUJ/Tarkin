98fd366ef91cd807382882bfec92ab4ea8a48136
Bartosz Zbik <bartosz@zbik.org>
2022-11-15 14:27:22 +0100

    support loading extensions

--- bot_actions/my_name.py
+++ bot_actions/my_name.py
@@ -1,8 +1,13 @@
 import discord
+from discord.ext import commands
 
 
-class MyName:
+class MyName(commands.Cog):
     """Sends users ID"""
 
     async def action_myname(self, message: discord.Message, core_message: str) -> None:
         await message.channel.send(f"Your name is: {message.author.name}")
+
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(MyName())
